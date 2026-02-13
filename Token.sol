d72bf184f4d977c6ad6af2203ea9e9b0c3611e5d
Akash <161524961+akash8987@users.noreply.github.com>
2026-02-13 14:01:31 +0600

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SimpleToken is ERC20 {
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     * @param name Name of the token
+     * @param symbol Symbol of the token
+     * @param initialSupply Total amount of tokens to mint (in wei)
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
